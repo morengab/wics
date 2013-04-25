c74e4ee59d555b06415a05a6c68174ce00bd502f
gaby <morengab@gmail.com>
2013-04-24 22:33:21 -0400

    Facebook login pulls full profile information including picture
    Had to do two separate get requests, would be better to do it in one
    but it was giving me trouble if i tried to get both profile fields and
    the picture. weird

--- server/accounts.js
+++ server/accounts.js
@@ -1,7 +1,21 @@
 Accounts.onCreateUser(function (options, user) {
     var accessToken = user.services.facebook.accessToken, 
         result, 
+        picture,
         profile;
+
+    result = Meteor.http.get("https://graph.facebook.com/me", {
+        params: {
+            access_token: accessToken,
+            fields: 'picture'
+        }
+    });
+
+    if (result.error) { throw result.error; }
+    
+    picture = _.pick(result.data,
+        "picture"
+    );
 
     result = Meteor.http.get("https://graph.facebook.com/me", {
         params: {
@@ -9,9 +23,7 @@ Accounts.onCreateUser(function (options, user) {
         }
     });
 
-    if (result.error) {
-        throw result.error;
-    }
+    if (result.error) { throw result.error; }
         
     profile = _.pick(result.data, 
         "id",
@@ -24,11 +36,13 @@ Accounts.onCreateUser(function (options, user) {
         "email",
         "hometown",
         "location",
+        "picture",
         "website",
         "work"
         );
 
     user.profile = profile;
+    user.profile['picture'] = picture['picture'];
 
     return user;
 });
