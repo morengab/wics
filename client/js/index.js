5a0cca9aad68bec75c6a5be1bd539a6983847c96
gaby <morengab@gmail.com>
2013-04-25 00:26:56 -0400

    Styling of thumbs as well as hovering effects
    Will do like/unlike and save/saved states now

--- client/js/index.js
+++ client/js/index.js
@@ -126,6 +126,104 @@ Template.home_page.event_thumbs = function () {
   }
 };
 
+
+/********************************************************
+* Event Thumbs
+*
+*/
+Template.event_thumb.helpers({
+
+  // Returns the name of the event creator
+  // If creator has no name, return email address
+  user_name: function () {
+    var event_owner = Meteor.users.findOne(this.owner);
+    if (event_owner.profile && event_owner.profile.name)
+      return event_owner.profile.name;
+    else 
+      return event_owner.emails[0].address;
+  }
+});
+
+Template.event_thumb.events({
+  'mouseenter': function (evt) {
+    var sel = "[data-id='"+this._id+"'] .btn";
+    $(sel).css('opacity', '1');
+  },
+
+  'mouseleave': function () {
+    var sel = "[data-id='"+this._id+"'] .btn";
+    $(sel).css('opacity', '0');
+  },
+
+  // Adds an event to the user's history of likes
+  'click .like': function () {
+    var e = this;
+    Events.update(
+      e._id, 
+      {
+        $push: { likes: { userId: Meteor.userId() }}
+      });
+
+    Meteor.users.update(
+      Meteor.userId(), 
+      {
+        $push: { "profile.likes": { eventId: e._id, timestamp: Date.now() }}
+      }
+    );
+  },
+
+  // Saves an event to the user's list of templates
+  'click .save': function () {
+    var orig_event = this;
+    
+    var title = orig_event.title;
+    var date = orig_event.date;
+    var description = orig_event.description;
+    var cost = orig_event.cost;
+    var planning = orig_event.planning;
+    var mode = 'template';
+    var image_url = orig_event.image_url;
+    var history = orig_event._id;
+
+    if (title.length) {
+      Meteor.call('createEvent', {
+        title: title,
+        date: date,
+        description: description,
+        cost: cost,
+        planning: planning,
+        mode: mode,
+        image_url: image_url,
+        history: history
+      }, function (error, new_event_id) {
+        if (! error) {
+          Events.update(
+            orig_event._id, 
+            {
+              $push: { 
+                saves: {
+                  user_id: Meteor.userId(),
+                  event_id: new_event_id,
+                  timestamp: Date.now() }}
+            }
+          );
+        }
+      });
+    } 
+    else {
+      // error checking goes here
+    }    
+  },
+
+  //TODO: limit saves and likes to one per user
+
+  // Deletes an event 
+  'click .delete': function () {
+    Events.remove(this._id);
+  }
+});
+
+
 /**
 * Routing Setup
 * 
